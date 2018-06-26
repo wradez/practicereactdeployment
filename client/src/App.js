27119a2509efccf56a4b7e431b996f9f81c7e2ea
Brook Trout <walkerradez@gmail.com>
2018-06-26 13:57:56 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Books} />
+        <Route exact path='/books' component={Books} />
+        <Route exact path='/books/:id' component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Fragment>
+  </Router>
+);
+
+export default App;
